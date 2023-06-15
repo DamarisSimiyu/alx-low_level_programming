4d5baf4fa1c64d0b140f91c0f4a6a958dfa982e5
DamarisSimiyu <damadeesimiyu@gmail.com>
2023-06-15 03:42:41 -0700

    b

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the message "but the outcome is a piece of art,"
+ *              followed by a new line, using printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("but the outcome is a piece of art,\n");
+	return (0);
+}
+
