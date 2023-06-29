909770da2dfcb8c34d1706af742e2417ee357ae5
DamarisSimiyu <damadeesimiyu@gmail.com>
2023-06-28 23:47:01 -0700

    d

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - Function that encodes a string into 1337
+ * @s: String container
+ *
+ * Return: String encode in 1337 form
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == s[i])
+				s[i] = b[j];
+		}
+	}
+	return (s);
+}
